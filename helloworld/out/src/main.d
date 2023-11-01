d502e821dad46f1d9f17ea1e2ffb8e6fdf421b63
Darkshadows61 <bisomt@gmail.com>
2023-11-01 14:51:15 -0400

    first

---
+++ helloworld/out/src/main.d
@@ -0,0 +1,13 @@
+out/src/main.o: src/main.c S:/sgdk/inc/genesis.h S:/sgdk/inc/types.h \
+ S:/sgdk/inc/config.h S:/sgdk/inc/asm.h S:/sgdk/inc/sys.h \
+ S:/sgdk/inc/sram.h S:/sgdk/inc/mapper.h S:/sgdk/inc/memory.h \
+ S:/sgdk/inc/tools.h S:/sgdk/inc/bmp.h S:/sgdk/inc/maths.h \
+ S:/sgdk/inc/vdp.h S:/sgdk/inc/pal.h S:/sgdk/inc/dma.h \
+ S:/sgdk/inc/vdp_tile.h S:/sgdk/inc/vdp_bg.h S:/sgdk/inc/map.h \
+ S:/sgdk/inc/font.h S:/sgdk/res/libres.h S:/sgdk/inc/string.h \
+ S:/sgdk/inc/tab_cnv.h S:/sgdk/inc/maths3D.h S:/sgdk/inc/vdp_dma.h \
+ S:/sgdk/inc/vdp_spr.h S:/sgdk/inc/vdp_pal.h S:/sgdk/inc/vram.h \
+ S:/sgdk/inc/sprite_eng.h S:/sgdk/inc/sound.h S:/sgdk/inc/xgm.h \
+ S:/sgdk/inc/z80_ctrl.h S:/sgdk/inc/ym2612.h S:/sgdk/inc/psg.h \
+ S:/sgdk/inc/joy.h S:/sgdk/inc/timer.h S:/sgdk/inc/task.h \
+ S:/sgdk/inc/task_cst.h
